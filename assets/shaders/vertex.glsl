3b46f19685e6b68f2afa9f98e82f46eb822b9232
tkzc00 <2238983030@qq.com>
2024-06-16 10:17:16 +0800

    控制三角形运动的速度

--- assets/shaders/vertex.glsl
+++ assets/shaders/vertex.glsl
@@ -3,13 +3,14 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
 uniform float time;
+uniform float speed;
 
 out vec3 color;
 
 void main()
 {
     float dx = 0.3;
-    float offsetX = sin(time) * dx;
+    float offsetX = sin(time * speed) * dx;
     gl_Position = vec4(aPos.x + offsetX, aPos.y, aPos.z, 1.0);
     color = aColor;
 }
--- main.cpp
+++ main.cpp
@@ -77,6 +77,7 @@ void render() {
     shader->begin();
     // 设置uniform变量
     shader->setFloat("time", (float) glfwGetTime());
+    shader->setFloat("speed", 10.0f);
     // 绑定VAO
     GL_CALL(glBindVertexArray(vao));
     // 发出绘制指令
