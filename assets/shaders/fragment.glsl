5fed211c5cb31aad384b9f0dfc710f997a17d030
tkzc00 <2238983030@qq.com>
2024-06-16 09:55:20 +0800

    通过fs实现颜色跟随时间动态变化

--- assets/shaders/fragment.glsl
+++ assets/shaders/fragment.glsl
@@ -1,8 +1,11 @@
 #version 460 core
 out vec4 FragColor;
+
+uniform float time;
+
 in vec3 color;
 
 void main()
 {
-    FragColor = vec4(color, 1.0);
+    FragColor = vec4(color, 1.0) * (sin(time) + 1.0) / 2.0;
 }
--- assets/shaders/vertex.glsl
+++ assets/shaders/vertex.glsl
@@ -2,12 +2,10 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
-uniform float time;
-
 out vec3 color;
 
 void main()
 {
     gl_Position = vec4(aPos, 1.0);
-    color = aColor * (sin(time) + 1.0) / 2.0;
+    color = aColor;
 }
