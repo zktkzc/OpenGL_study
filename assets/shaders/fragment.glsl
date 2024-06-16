121c97518002289054b21c3ef8db89d2b6ecc9d8
tkzc00 <2238983030@qq.com>
2024-06-16 10:09:50 +0800

    NDC作为颜色输出

--- assets/shaders/fragment.glsl
+++ assets/shaders/fragment.glsl
@@ -1,12 +1,10 @@
 #version 460 core
 out vec4 FragColor;
 
-uniform float time;
-
 in vec3 color;
+in vec3 position;
 
 void main()
 {
-    float intensity = (sin(time) + 1.0) / 2.0;
-    FragColor = vec4(vec3(intensity) + color, 1.0);
+    FragColor = vec4(position, 1.0);
 }
--- assets/shaders/vertex.glsl
+++ assets/shaders/vertex.glsl
@@ -2,12 +2,12 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
-uniform float time;
-
 out vec3 color;
+out vec3 position;
 
 void main()
 {
     gl_Position = vec4(aPos, 1.0);
-    color = aColor * (cos(time) + 1.0) / 2.0;
+    color = aColor;
+    position = aPos;
 }
